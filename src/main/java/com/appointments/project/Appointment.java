1ce31868e3dc9eb8a7ce8db2be8771ede8e71eeb
kafonin <afonk@mail.broward.edu>
2020-03-18 23:05:29 -0400

    CSS Test

---
+++ src/main/java/com/appointments/project/Appointment.java
@@ -0,0 +1,89 @@
+package com.appointments.project;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Appointment
+ */
+public class Appointment 
+{
+    private @Id @GeneratedValue Long id;
+    private String  firstNameString;
+    private String  lastNameString;
+    private String  carBrandString;
+    private String  carModelString;
+    private int     carYearInt;
+
+    private Appointment() {}
+
+    public Appointment(String firstNameString, String lastNameString, String carBrandString, 
+                        String carModelString, int carYearInt)
+    {
+        this.firstNameString    =   firstNameString;
+        this.lastNameString     =   lastNameString;
+        this.carBrandString     =   carBrandString;
+        this.carModelString     =   carModelString;
+        this.carYearInt         =   carYearInt;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id) 
+    {
+        this.id = id;
+    }
+    
+    public String getFirstNameString() 
+    {
+        return firstNameString;
+    }
+    
+    public void setFirstNameString(String firstNameString)
+    {
+        this.firstNameString = firstNameString;
+    }
+
+    public String getLastNameString()
+    {
+        return lastNameString;
+    }
+
+    public void setLastNameString(String lastNameString) 
+    {
+        this.lastNameString = lastNameString;
+    }
+
+    public String setCarBrandString()
+    {
+        return carBrandString;
+    }
+
+    public void getCarBrandString(String carBrandString)
+    {
+        this.carBrandString = carBrandString;
+    }
+
+    public String getCarModelString() 
+    {
+        return carModelString;
+    }
+
+    public void setCarModelString(String carModelString) 
+    {
+        this.carModelString = carModelString;
+    }
+
+    public int getCarYearInt()
+    {
+        return carYearInt;
+    }
+
+    public void setCarYearInt(int carYearInt) 
+    {
+        this.carYearInt = carYearInt;
+    }
+}
---
+++ src/main/java/com/appointments/project/HomeController.java
@@ -0,0 +1,16 @@
+package com.appointments.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * HomeController
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+}
